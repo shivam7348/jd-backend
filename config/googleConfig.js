b439eaf80c36d08ceba6d21c161cc55dd00795c8
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2025-02-24 00:21:15 +0530

    some unneccessary file deleted

--- config/googleConfig.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import { google } from "googleapis";
-
-export const oauth2client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  "postmessage"
-);
